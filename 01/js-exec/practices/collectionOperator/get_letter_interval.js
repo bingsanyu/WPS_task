ac5c680ac4454c5021265e6556321a84eff519d6
bingsanyu <12348372+bingsanyu@user.noreply.gitee.com~>
2024-01-05 21:20:43 +0800

    Solved 01(60/80)-collectionOperator(17/17)

--- 01/js-exec/practices/collectionOperator/get_letter_interval.js
+++ 01/js-exec/practices/collectionOperator/get_letter_interval.js
@@ -2,6 +2,17 @@
 
 function get_letter_interval(number_a, number_b) {
   //在这里写入代码
+  let result = [];
+  if (number_a <= number_b) {
+    for (let i = number_a; i <= number_b; i++) {
+      result.push(String.fromCharCode(96 + i));
+    }
+  } else {
+    for (let i = number_a; i >= number_b; i--) {
+      result.push(String.fromCharCode(96 + i));
+    }
+  }
+  return result;
 }
 
 module.exports = get_letter_interval;
--- 01/js-exec/practices/collectionOperator/get_letter_interval_2.js
+++ 01/js-exec/practices/collectionOperator/get_letter_interval_2.js
@@ -2,6 +2,27 @@
 
 function get_letter_interval_2(number_a, number_b) {
   //在这里写入代码
+  function toLetters(num) {
+    let result = '';
+    while (num > 0) {
+      let mod = (num - 1) % 26;
+      result = String.fromCharCode(97 + mod) + result;
+      num = Math.floor((num - mod) / 26);
+    }
+    return result;
+  }
+
+  let result = [];
+  if (number_a <= number_b) {
+    for (let i = number_a; i <= number_b; i++) {
+      result.push(toLetters(i));
+    }
+  } else {
+    for (let i = number_a; i >= number_b; i--) {
+      result.push(toLetters(i));
+    }
+  }
+  return result;
 }
 
 module.exports = get_letter_interval_2;
