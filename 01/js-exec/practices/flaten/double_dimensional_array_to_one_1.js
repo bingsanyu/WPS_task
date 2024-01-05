27292bf3629b1993420310c84c2244fbcace8122
bingsanyu <12348372+bingsanyu@user.noreply.gitee.com~>
2024-01-05 20:40:37 +0800

    Solved 01(43/80)-flaten(2/2)

--- 01/js-exec/practices/flaten/double_dimensional_array_to_one_1.js
+++ 01/js-exec/practices/flaten/double_dimensional_array_to_one_1.js
@@ -3,6 +3,17 @@
 function double_to_one(collection) {
 
   //在这里写入代码
+  let result = [];
+  collection.forEach(function (item) {
+    if (typeof item === 'number') {
+      result.push(item);
+    } else {
+      item.forEach(function (item) {
+        result.push(item);
+      });
+    }
+  });
+  return result;
 }
 
 module.exports = double_to_one;
--- 01/js-exec/practices/flaten/double_dimensional_array_to_one_2.js
+++ 01/js-exec/practices/flaten/double_dimensional_array_to_one_2.js
@@ -3,6 +3,9 @@
 function double_to_one(collection) {
 
   //在这里写入代码
+  return collection.reduce((result, arr) => {
+    return result.concat(arr.filter(item => !result.includes(item)));
+  }, []);
 }
 
 module.exports = double_to_one;
