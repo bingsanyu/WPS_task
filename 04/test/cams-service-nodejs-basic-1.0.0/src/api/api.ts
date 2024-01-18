eabcbe05b16f89f1919c4c75a5b753c4195c61b8
bingsanyu <12348372+bingsanyu@user.noreply.gitee.com~>
2024-01-19 04:52:34 +0800

    Solved 04

--- 04/test/cams-service-nodejs-basic-1.0.0/src/api/api.ts
+++ 04/test/cams-service-nodejs-basic-1.0.0/src/api/api.ts
@@ -1,9 +1,10 @@
+import axios from 'axios';
 import { Context } from 'koa'
 const Router = require('@koa/router')
 
 export function api() {
   const router = new Router()
-  // 指定路由
+  
   router.get('/api/test/ping', async function (ctx: Context) {
     ctx.status = 200
     ctx.body = {
@@ -11,6 +12,33 @@ export function api() {
     }
     return ctx
   })
+
+  const baseUrl = 'http://encs-pri-cams-engine/i/docmini';
+
+  router.post('/api/dept/create', async function (ctx: Context) {
+    const { name, companyId } = ctx.request.body;
+  
+    // 获取根部门
+    const rootDeptRes = await axios.get(`${baseUrl}/org/dev/v1/companies/${companyId}/depts/root`);
+    if (rootDeptRes.status !== 200) {
+      ctx.status = rootDeptRes.status;
+      ctx.body = rootDeptRes.data;
+      return ctx;
+    }
+    const rootDeptId = rootDeptRes.data.data.id;
+  
+    // 创建子部门
+    const createDeptRes = await axios.post(`${baseUrl}/org/dev/v1/companies/${companyId}/depts/${rootDeptId}`, {
+      name: name
+    }, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    ctx.status = createDeptRes.status;
+    ctx.body = createDeptRes.data;
+    return ctx;
+  });
   return router
 }
 
